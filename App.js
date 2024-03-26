59384c89f5d2f50752f326ef3e7893707375fff5
Ana Clara <ana.oliveira33@estudante.ifms.edu.br>
2024-03-26 12:22:43 -0400

    forma 02 de fazer

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import {View, Text, Image} from "react-native";
 
 // snack.expo.dev
-function App() {
-    return (
-        // view é uma tela ou fragmento de tela
-        <View>
-            <Text>Olá Mundo!</Text>
-        </View>
-    )
+class App extends Component() {
+    render() {
+        let curso = 'INFO';
+
+        return (
+            // view é uma tela ou fragmento de tela
+            <View>
+                <Text>Olá Mundo!</Text>
+                <Text stryle={{color: 'blue', fontSize: 30, margin: 15}}>Olá Mundo!</Text>
+                <Image source={{uri: 'url'}} style={{width: 300, height: 300,}} />
+                <Text>{curso}</Text>
+            </View>
+        )
+    }
 }
 
 export default App;
