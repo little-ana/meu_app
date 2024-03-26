9f3b29344bc3f12e0cf437ce42ae25525b53969a
Ana Clara <ana.oliveira33@estudante.ifms.edu.br>
2024-03-26 12:09:25 -0400

    forma 01 de fazer

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, Image} from "react-native";
+
+// snack.expo.dev
+function App() {
+    return (
+        // view é uma tela ou fragmento de tela
+        <View>
+            <Text>Olá Mundo!</Text>
+        </View>
+    )
+}
+
+export default App;
